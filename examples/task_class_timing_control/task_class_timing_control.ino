81111e3939e71aeb02ec81a0ea68d95133bb9696
Hideaki Tai <hideaki.tai@gmail.com>
2021-04-07 19:35:26 +0900

    update examples

--- examples/task_class_timing_control/task_class_timing_control.ino
+++ examples/task_class_timing_control/task_class_timing_control.ino
@@ -19,66 +19,64 @@ void setup() {
     Serial.println(Tasks.size());
     Serial.print("active tasks are ");
     Serial.println(Tasks.getActiveTaskSize());
-    // Serial.print("auto erase : ");
-    // Serial.println(Tasks.isAutoErase() ? "true" : "false");
+    Serial.print("auto erase : ");
+    Serial.println(Tasks.isAutoErase() ? "true" : "false");
 
-    // Serial.println("main loop start");
+    Serial.println("main loop start");
 }
 
 void loop() {
-    delay(1000);
-    Serial.println("loop");
-    // Tasks.update();
+    Tasks.update();
 
-    // if (Tasks.frame("task1") >= 5 && !Tasks.isRunning("task2")) {
-    //     Serial.print("all tasks are ");
-    //     Serial.println(Tasks.size());
-    //     Serial.print("active tasks are ");
-    //     Serial.println(Tasks.getActiveTaskSize());
+    if (Tasks.frame("task1") >= 5 && !Tasks.isRunning("task2")) {
+        Serial.print("all tasks are ");
+        Serial.println(Tasks.size());
+        Serial.print("active tasks are ");
+        Serial.println(Tasks.getActiveTaskSize());
 
-    //     Serial.println("start task2");
-    //     Tasks.start("task2");
-    // }
+        Serial.println("start task2");
+        Tasks.start("task2");
+    }
 
-    // if (Tasks.frame("task1") >= 10) {
-    //     Serial.print("all tasks are ");
-    //     Serial.println(Tasks.size());
-    //     Serial.print("active tasks are ");
-    //     Serial.println(Tasks.getActiveTaskSize());
+    if (Tasks.frame("task1") >= 10) {
+        Serial.print("all tasks are ");
+        Serial.println(Tasks.size());
+        Serial.print("active tasks are ");
+        Serial.println(Tasks.getActiveTaskSize());
 
-    //     Serial.println("stop task1");
-    //     Tasks.stop("task1");
-    // }
+        Serial.println("stop task1");
+        Tasks.stop("task1");
+    }
 
-    // if (Tasks.frame("task2") >= 15) {
-    //     Serial.print("all tasks are ");
-    //     Serial.println(Tasks.size());
-    //     Serial.print("active tasks are ");
-    //     Serial.println(Tasks.getActiveTaskSize());
+    if (Tasks.frame("task2") >= 15) {
+        Serial.print("all tasks are ");
+        Serial.println(Tasks.size());
+        Serial.print("active tasks are ");
+        Serial.println(Tasks.getActiveTaskSize());
 
-    //     Serial.println("stop task2");
-    //     Tasks.stop("task2");
-    // }
+        Serial.println("stop task2");
+        Tasks.stop("task2");
+    }
 
-    // if (millis() > 16000 && !Tasks.empty()) {
-    //     Serial.print("all tasks are ");
-    //     Serial.println(Tasks.size());
-    //     Serial.print("active tasks are ");
-    //     Serial.println(Tasks.getActiveTaskSize());
+    if (millis() > 16000 && !Tasks.empty()) {
+        Serial.print("all tasks are ");
+        Serial.println(Tasks.size());
+        Serial.print("active tasks are ");
+        Serial.println(Tasks.getActiveTaskSize());
 
-    //     Serial.println("erase task1");
-    //     Tasks.erase("task1");
-    //     Serial.println("erase task2");
-    //     Tasks.erase("task2");
+        Serial.println("erase task1");
+        Tasks.erase("task1");
+        Serial.println("erase task2");
+        Tasks.erase("task2");
 
-    //     Serial.print("all tasks are ");
-    //     Serial.println(Tasks.size());
-    //     Serial.print("active tasks are ");
-    //     Serial.println(Tasks.getActiveTaskSize());
-    // }
+        Serial.print("all tasks are ");
+        Serial.println(Tasks.size());
+        Serial.print("active tasks are ");
+        Serial.println(Tasks.getActiveTaskSize());
+    }
 
-    // if (millis() > 17000) {
-    //     Serial.println("end");
-    //     delay(1000);
-    // }
+    if (millis() > 17000) {
+        Serial.println("end");
+        delay(1000);
+    }
 }
--- examples/task_func_basic/task_func_basic.ino
+++ examples/task_func_basic/task_func_basic.ino
@@ -18,37 +18,42 @@ void setup() {
         ->start();
 
     // task framerate is 1 and repeat forever
-    // Tasks.add([]{
-    //     Serial.print("framerate forever task: now = ");
-    //     Serial.println(millis());
-    // })->startFps(1);
+    // Tasks.add([]() {
+    //          Serial.print("framerate forever task: now = ");
+    //          Serial.println(millis());
+    //      })
+    //     ->startFps(1);
 
     // task framerate is 1 and repeat forever (pre-defined function)
     // Tasks.add(my_task)->startFps(1);
 
     // task framerate is 2 and 10 times only
-    // Tasks.add([]{
-    //     Serial.print("framerate limited task: now = ");
-    //     Serial.println(millis());
-    // })->startFpsFor(2, 10);
+    // Tasks.add([] {
+    //          Serial.print("framerate limited task: now = ");
+    //          Serial.println(millis());
+    //      })
+    //     ->startFpsFor(2, 10);
 
     // task interval is 1.0[sec] and repeat forever
-    // Tasks.add([]{
-    //     Serial.print("interval forever task: now = ");
-    //     Serial.println(millis());
-    // })->startInterval(1.0);
+    // Tasks.add([] {
+    //          Serial.print("interval forever task: now = ");
+    //          Serial.println(millis());
+    //      })
+    //     ->startInterval(1.0);
 
     // task interval is 0.5[sec] and 10 times only
-    // Tasks.add([]{
-    //     Serial.print("interval limited task: now = ");
-    //     Serial.println(millis());
-    // })->startIntervalFor(0.5, 10);
+    // Tasks.add([] {
+    //          Serial.print("interval limited task: now = ");
+    //          Serial.println(millis());
+    //      })
+    //     ->startIntervalFor(0.5, 10);
 
     // task is executed only once after 5.0[ms]
-    // Tasks.add([]{
-    //     Serial.print("once task: now = ");
-    //     Serial.println(millis());
-    // })->startOnceAfter(5.0);
+    // Tasks.add([] {
+    //          Serial.print("once task: now = ");
+    //          Serial.println(millis());
+    //      })
+    //     ->startOnceAfter(5.0);
 
     Serial.print("task start: now = ");
     Serial.println(millis());
