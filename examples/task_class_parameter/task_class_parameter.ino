11d0c1614a0f85ab813c82157643825063beedaf
Hideaki Tai <hideaki.tai@gmail.com>
2021-08-23 18:18:27 +0900

    Update examples

---
+++ examples/task_class_parameter/task_class_parameter.ino
@@ -0,0 +1,13 @@
+#include <TaskManager.h>
+#include "Speak.h"
+
+void setup() {
+    delay(2000);
+    Tasks.add<Speak>("speak")
+        ->number(123)  // you can set required parameter like this
+        ->startInterval(0.5);
+}
+
+void loop() {
+    Tasks.update();
+}
--- examples/task_class_timing_control/task_class_timing_control.ino
+++ examples/task_class_timing_control/task_class_timing_control.ino
@@ -77,6 +77,7 @@ void loop() {
 
     if (millis() > 11000) {
         Serial.println("end");
-        delay(1000);
+        while (true)
+            ;
     }
 }
