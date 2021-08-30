8e39f4c30fa31872b9b56a1921f6c3517b63ef86
Hideaki Tai <hideaki.tai@gmail.com>
2021-08-30 13:37:27 +0900

    Add support for subtasks

--- TaskManager/TaskBase.h
+++ TaskManager/TaskBase.h
@@ -8,6 +8,11 @@
 namespace arduino {
 namespace task {
 
+    enum class SubTaskMode : uint8_t {
+        SYNC,
+        SEQUENCE
+    };
+
     class Manager;
 
     class Base : public FrameRateCounter {
@@ -16,6 +21,13 @@ namespace task {
     protected:
         String name;
         bool b_auto_erase {false};
+
+        // for SubTask
+        Vec<Ref<Base>> subtasks;
+        SubTaskMode mode {SubTaskMode::SYNC};
+        // only for SubTaskMode::SEQUENCE
+        uint8_t subtask_index {0};
+        Vec<double> subtask_durations;
 
     public:
         Base(const String& name)
@@ -33,16 +45,90 @@ namespace task {
         virtual void idle() {};
         virtual void reset() {};
 
-        bool hasEnter() const { return this->hasStarted(); }
-        bool hasExit() const { return this->hasStopped(); }
+        bool hasEnter() const {
+            return this->hasStarted();
+        }
+        bool hasExit() const {
+            return this->hasStopped();
+        }
 
         Base* setAutoErase(const bool b) {
             b_auto_erase = b;
             return this;
         }
-        bool isAutoErase() const { return b_auto_erase; }
+        bool isAutoErase() const {
+            return b_auto_erase;
+        }
 
-        const String& getName() const { return name; }
+        const String& getName() const {
+            return name;
+        }
+
+        // =========== for SubTask ==========
+
+        template <typename TaskType>
+        Base* subtask(const String& name, std::function<void(Ref<TaskType>)> setup) {
+            Ref<TaskType> t = std::make_shared<TaskType>(name);
+            subtasks.emplace_back(t);
+            t->begin();
+            setup(t);
+            subtask_durations.emplace_back(t->getDurationUsec());
+            return this;
+        }
+
+        Vec<Ref<Base>>& getSubTasks() {
+            return subtasks;
+        }
+        const Vec<Ref<Base>>& getSubTasks() const {
+            return subtasks;
+        }
+
+        Base* setSubTaskIndex(const size_t i) {
+            subtask_index = i;
+            return this;
+        }
+        size_t getSubTaskIndex() const {
+            if (mode == SubTaskMode::SEQUENCE)
+                return subtask_index;
+            else
+                return 0;
+        }
+
+        Base* setSubTaskMode(const SubTaskMode m) {
+            mode = m;
+            return this;
+        }
+        SubTaskMode getSubTaskMode() const {
+            return mode;
+        }
+
+        bool hasSubTasks() const {
+            return !subtasks.empty();
+        }
+
+        size_t numSubTasks() const {
+            return subtasks.size();
+        }
+
+        // ========== only for SubTaskMode::SEQUENCE ==========
+
+        double getCurrentDurationSec() const {
+            if (mode == SubTaskMode::SEQUENCE)
+                return subtasks[subtask_index]->getDurationSec();
+            else
+                return 0.;
+        }
+
+        double getCurrentDurationSecSum() const {
+            if (mode == SubTaskMode::SEQUENCE) {
+                double d = 0.;
+                for (size_t i = 0; i < subtask_index; ++i)
+                    d += subtasks[i]->getDurationSec();
+                return d;
+            } else {
+                return 0.;
+            }
+        }
     };
 
 }  // namespace task
